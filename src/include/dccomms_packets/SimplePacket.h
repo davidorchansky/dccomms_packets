6e91cfa5b29f9ac2f3679f6f9514e659e0a0f4e4
dcentelles <centelld@uji.es>
2018-01-20 19:58:24 +0100

    add a copy of the wireless_ardusub::SimplePacket class as an example (from the wireless_ardusub project)

---
+++ src/include/dccomms_packets/SimplePacket.h
@@ -0,0 +1,62 @@
+/*
+ * This packet is a copy of wireless_ardusub::SimplePacket (wireless_ardusub project)
+ */
+
+#ifndef DCCOMMS_PACKETS_SIMPLEPACKET_H_
+#define DCCOMMS_PACKETS_SIMPLEPACKET_H_
+
+#include <dccomms/dccomms.h>
+#include <dccomms_packets/types.h>
+
+using namespace dccomms;
+namespace dccomms_packets {
+
+class SimplePacket : public Packet {
+public:
+  SimplePacket(int payloadSize, FCS fcs = CRC16);
+  void CopyFromRawBuffer(void *buffer);
+  uint8_t *GetPayloadBuffer();
+  uint32_t GetPayloadSize();
+  int GetPacketSize();
+  void Read(Stream *comms);
+  void PayloadUpdated(uint32_t payloadSize);
+
+  bool PacketIsOk();
+
+  void GetPayload(void *copy, int size);
+  void SetPayload(const void *data, int size);
+
+
+  void UpdateFCS();
+
+private:
+  int PAYLOAD_SIZE, FCS_SIZE;
+  static const int PRE_SIZE = 1;
+
+  uint8_t *_pre;
+  uint8_t *_payload;
+  uint8_t *_fcs;
+  int _packetSize;
+  void _Init();
+  bool _CheckFCS();
+};
+
+class SimplePacketBuilder : public IPacketBuilder {
+public:
+  SimplePacketBuilder(int payloadSize, FCS fcs = CRC16) {
+    _payloadSize = payloadSize;
+    _fcs = fcs;
+  }
+  PacketPtr CreateFromBuffer(void *buffer) {
+    auto pkt = CreateObject<SimplePacket>(_payloadSize, _fcs);
+    pkt->CopyFromRawBuffer(buffer);
+    return pkt;
+  }
+  PacketPtr Create() { return CreateObject<SimplePacket>(_payloadSize, _fcs); }
+
+private:
+  int _payloadSize;
+  FCS _fcs;
+};
+}
+#endif //DCCOMMS_PACKETS_SIMPLEPACKET_H_
